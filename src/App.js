7bd0ae2867b76b0f2d8b6ef2c327d76884bab719
Edwin Oswaldo Flores Gutierrez <46845817+EdwinFlores29@users.noreply.github.com>
2024-11-17 19:16:56 -0600

    agregando el nav
    con estilos Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import ListadoEmpleados from "./components/ListadoEmpleados";
+import Navegacion from "./plantilla/Navegacion";
 
 function App() {
   return (
-    <ListadoEmpleados />
+      <div className="container">
+          <Navegacion />
+          <ListadoEmpleados />
+      </div>
+
   );
 }
 
