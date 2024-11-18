1522a026b55227ce63e705df5cd25ea0cf018608
Edwin Oswaldo Flores Gutierrez <46845817+EdwinFlores29@users.noreply.github.com>
2024-11-18 17:38:45 -0600

    Agregar mas funcionalidad
    para modificar el usuario

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import ListadoEmpleados from "./components/ListadoEmpleados";
 import Navegacion from "./plantilla/Navegacion";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import AgregarEmpleado from "./components/AgregarEmpleado";
+import EditarEmpleado from "./components/EditarEmpleado";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
               <Routes>
                   <Route exact path="/" element={<ListadoEmpleados />} />
                   <Route exact path="/agregar" element={<AgregarEmpleado />} />
+                  <Route exact path="/editar/:id" element={<EditarEmpleado />} />
               </Routes>
           </BrowserRouter>
       </div>
--- src/components/ListadoEmpleados.jsx
+++ src/components/ListadoEmpleados.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import {NumericFormat} from "react-number-format";
+import {Link} from "react-router-dom";
 
 export default function ListadoEmpleados() {
 
@@ -33,6 +34,7 @@ export default function ListadoEmpleados() {
                     <th scope="col">Empleado</th>
                     <th scope="col">Departamento</th>
                     <th scope="col">Sueldo</th>
+                    <th></th>
                 </tr>
                 </thead>
                 <tbody>
@@ -49,6 +51,12 @@ export default function ListadoEmpleados() {
                             prefix={'C$'}
                             decimalScale={2}
                             fixedDecimalScale /></td>
+                            <td className="text-center">
+                                <div>
+                                    <Link to={`/editar/${empleado.idEmpleado}`}
+                                          className="btn btn-warning btn-sm me-3">Editar</Link>
+                                </div>
+                            </td>
                         </tr>
                     ))
                 }
